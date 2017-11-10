fb0dc64778f326b81c9512b30a26f4c7b66241b9
diegoarmando1997 <31254624+diegoarmando1997@users.noreply.github.com>
2017-11-10 13:41:39 -0600

    Add files via upload
    El diagrama recorre desde el 1 hasta el 5 aumnetando en dos unidades e imprime aquellos números que no son múltiplos de 3.
    En este caso, sólo el 1 y el 5.

---
+++ diagrama.cc
@@ -0,0 +1,15 @@
+//EL diagrama recorre del 1 al 5 aumentado 2 unidades e imprime los número que no sean múltiplos del 3, en este caso, sólo el 1 y el 5.
+#include <iostream>
+using namespace std;
+
+int main(){
+  for (int x=1; x<=5; x=x+2){
+    if (x%3==0){}
+    else {
+      cout<<"x= "<<x<<endl;
+    }
+  }
+return 0;
+}
+  
+
